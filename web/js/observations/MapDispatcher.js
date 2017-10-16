b3bf42fe460d3fedf60fdbca6324c93c0e7ef6bf
Q <r.quevyn@live.fr>
2017-10-16 14:48:44 +0200

    Layouts d'observation rangeable: changement des vues

--- web/js/observations/MapDispatcher.js
+++ web/js/observations/MapDispatcher.js
@@ -1,16 +1,11 @@
 //Les données sont envoyées dans la vue dans une balise marquée par "coords". et séparée par un ';'
-
+//On ajoute les interactions entre carte et liste des observations ici.
 
 function OnMapReady(map)
 {
     $(function(){
        $('.coords').each(function(ind){
            var latLn = $(this).text().split(";");
-
-
-
-
-
           positionnerSurCarte(map,latLn[0],latLn[1]);
        });
     });
@@ -24,7 +19,7 @@ function positionnerSurCarte(map,latitude,longitude)
 
 
     var marker = new google.maps.Marker({
-        position: lat ,
+        position: lat,
         map: map,
         title: "Une obs"
     });
