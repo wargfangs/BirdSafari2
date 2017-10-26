eae828a635b04c0d07cde6b10ef9cee3b07a6698
Q <r.quevyn@live.fr>
2017-10-26 15:22:58 +0200

    administration, true commit

---
+++ web/js/observations/addressLoc.js
@@ -0,0 +1,15 @@
+$(function(){
+   var geocoder; // Geocoding: process by with we extrapolate addresses from coordinate.
+
+
+});
+
+
+function initialize(geocoder)
+{
+    geocoder = new google.maps.Geocoder();
+    //Récupération du point de la carte.
+
+    //Création de la carte avec comme point de référence le point au dessus. 
+
+}
